1318177fa72e1515675ddcd96072b9d33dd9e7ee
Kamry Bowman <kamry.bowman@gmail.com>
2019-04-22 15:22:52 -0600

    Refactor PalindromePermutation with bit vector

--- python/arrays-strings/PalindromePermutations.py
+++ python/arrays-strings/PalindromePermutations.py
@@ -2,13 +2,23 @@ from collections import Counter
 
 
 def checkPalindromePerm(string):
-    counter = Counter(string.lower().replace(" ", ""))
+
+    vector = 0
+    for char in string:
+        if char == " ":
+            continue
+        index = ord(char.lower()) - ord("a")
+
+        vector ^= 1 << index
+
+    if vector == 0:
+        return True
 
     odds = 0
-    print(counter)
-    for char, count in counter.items():
-        if count % 2 == 1:
+    while vector > 0:
+        if vector & 1:
             odds += 1
+        vector = vector >> 1
 
     return odds < 2
 
@@ -16,6 +26,9 @@ def checkPalindromePerm(string):
 if __name__ == "__main__":
     print(checkPalindromePerm("Tact Coa"))
     print(checkPalindromePerm("Tact Coaa"))
+    print(checkPalindromePerm("aa bb"))
+    print(checkPalindromePerm("aa bbb"))
+    print(checkPalindromePerm("aaa bbb"))
 
 # time: O(n)
-# space: O(n)
+# space: O(1)
